d97148f8a59785d33d84650321a5339e112a69d5
jgmorawetz <james.morawetz@gmail.com>
2025-03-05 23:56:08 -0500

    Added script for getting chains.

---
+++ scripts/generate_all_chains.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=MCMCchains
+#SBATCH --qos=regular
+#SBATCH --account=desi
+#SBATCH --constraint=cpu
+#SBATCH --ntasks=16
+#SBATCH --cpus-per-task=1
+#SBATCH --time=48:00:00
+
+module load julia
+
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="FS" --variation="LCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="FS" --variation="w0waCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="BAO" --variation="LCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="BAO" --variation="w0waCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="CMB" --variation="LCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="CMB" --variation="w0waCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="FS+BAO" --variation="LCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="FS+BAO" --variation="w0waCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="FS+BAO+CMB" --variation="LCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="FS+BAO+CMB" --variation="w0waCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="FS+BAO+CMB+DESY5SN" --variation="LCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="FS+BAO+CMB+DESY5SN" --variation="w0waCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="FS+BAO+CMB+PantheonPlusSN" --variation="LCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="FS+BAO+CMB+PantheonPlusSN" --variation="w0waCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="FS+BAO+CMB+Union3SN" --variation="LCDM" & 
+srun -n 1 -c 1 julia MCMC_chains_DESIY1FullShape.jl --dataset="FS+BAO+CMB+Union3SN" --variation="w0waCDM" & 
+
+wait
