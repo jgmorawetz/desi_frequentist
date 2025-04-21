f9490b0f6e17c4368510733886d4c2287fbb58aa
jgmorawetz <james.morawetz@gmail.com>
2025-04-20 23:01:12 -0400

    Job script for new MAP version.

--- scripts/generate_all_MAP.sh
+++ scripts/generate_all_MAP.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=MAPchain
-#SBATCH --account=rrg-wperciva
+#SBATCH --job-name=MAP
+#SBATCH --qos=shared
+#SBATCH --account=desi
+#SBATCH --constraint=cpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=30G
-#SBATCH --time=96:00:00
+#SBATCH --time=48:00:00
 #SBATCH --array=0-11
-
 
 PARAMS=(
     "FS LCDM"
@@ -27,5 +27,6 @@ PARAM_SET=(${PARAMS[$SLURM_ARRAY_TASK_ID]})
 n_runs=100
 dataset="${PARAM_SET[0]}"
 variation="${PARAM_SET[1]}"
+chains_path="/global/homes/j/jgmorawe/FrequentistExample1/FrequentistExample1/MCMC_results_paper/${dataset}_${variation}_5000_1000_0.65_x6merged_chain.npy"
 
-julia MAP_DESIY1FullShape.jl --n_runs $n_runs --dataset "$dataset" --variation "$variation"
+julia MAP_DESIY1FullShape.jl --n_runs $n_runs --dataset "$dataset" --variation "$variation" --chains_path "$chains_path"
